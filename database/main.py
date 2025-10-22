853ad371ce040933b197e81036e1ebd82841d79f
nelexey <codenamed.asyncz@gmail.com>
2025-10-22 22:20:34 +0300

    Фикс импортов по автотестам.

--- database/main.py
+++ database/main.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from sqlalchemy.engine import Engine
-from sqlalchemy.ext.declarative import declarative_base
 
 class Database:
 
